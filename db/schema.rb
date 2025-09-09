a9b38ecff150fb3eeb7a33c3b84f2d9d31cafc44
Eli Duke <elimduke@gmail.com>
2025-09-09 09:02:14 -0700

    copy and paste old schema for db:schema:load

---
+++ db/schema.rb
@@ -0,0 +1,147 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 0) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "bands", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.string "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "location"
+    t.string "bandcamp"
+    t.string "facebook"
+    t.string "soundcloud"
+    t.string "instagram"
+    t.string "twitter"
+    t.integer "status", default: 0, null: false
+    t.index ["status"], name: "index_bands_on_status"
+  end
+
+  create_table "bands_shows", force: :cascade do |t|
+    t.bigint "band_id"
+    t.bigint "show_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["band_id"], name: "index_bands_shows_on_band_id"
+    t.index ["show_id"], name: "index_bands_shows_on_show_id"
+  end
+
+  create_table "features", force: :cascade do |t|
+    t.string "name"
+    t.integer "target_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "caption"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
+  create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.bigint "venue_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date "date"
+    t.text "notes"
+    t.integer "status", default: 0, null: false
+    t.index ["status"], name: "index_shows_on_status"
+    t.index ["venue_id"], name: "index_shows_on_venue_id"
+  end
+
+  create_table "supporters", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
+    t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.text "info"
+    t.string "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "location"
+    t.string "facebook"
+    t.string "instagram"
+    t.string "twitter"
+    t.integer "status", default: 0, null: false
+    t.index ["status"], name: "index_venues_on_status"
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.bigint "band_id"
+    t.bigint "show_id"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "status", default: 0, null: false
+    t.index ["band_id"], name: "index_videos_on_band_id"
+    t.index ["show_id"], name: "index_videos_on_show_id"
+    t.index ["status"], name: "index_videos_on_status"
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bands_shows", "bands"
+  add_foreign_key "bands_shows", "shows"
+  add_foreign_key "shows", "venues"
+  add_foreign_key "videos", "bands"
+  add_foreign_key "videos", "shows"
+end
